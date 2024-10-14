7d7bd7cddf367114d0bfaeab511ac1aae7478dc5
Joaquin Incio <joaquin.incio2002@outlook.com>
2024-10-14 09:10:13 -0500

    Update

---
+++ Assets/Scripts/Player/VignetteController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VignetteController : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer vignette;
+    [Range(0f, 1f)]
+    [SerializeField] private float apertureSize;
+    [Range(0f, 1f)]
+    [SerializeField] private float featheringEffect;
+    [SerializeField] private Color vignetteColor = Color.red;
+    [SerializeField] private Color vignetteColorBlend = Color.black;
+
+    private MaterialPropertyBlock propertyBlock;
+    private Shader vignetteShader;
+    public bool active = false;
+    private bool alreadyactive = false;
+
+    private void Start()
+    {
+        propertyBlock = new MaterialPropertyBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        propertyBlock.SetFloat("_ApertureSize", apertureSize);
+        propertyBlock.SetFloat("_FeatheringEffect", featheringEffect);
+        propertyBlock.SetColor("_VignetteColor", vignetteColor);
+        propertyBlock.SetColor("_VignetteColorBlend", vignetteColorBlend);
+
+        vignette.SetPropertyBlock(propertyBlock);
+    }
+}
