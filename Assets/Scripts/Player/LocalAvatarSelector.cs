886af92cb71486fefefb7f1ae13b680fde84e19a
Alfredo Pacheco <67089831+Alpha23-coder@users.noreply.github.com>
2024-11-06 17:48:55 -0500

    Update

--- Assets/Scripts/Player/LocalAvatarSelector.cs
+++ Assets/Scripts/Player/LocalAvatarSelector.cs
@@ -7,15 +7,23 @@ public class LocalAvatarSelector : MonoBehaviour
     [Header("Dependencies")]
     [SerializeField] private GameObject[] rigs;
 
+    private void Awake()
+    {
+        for (int i = 0; i < rigs.Length; i++)
+        {
+            rigs[i].SetActive(false);
+        }
+    }
+
     void Start()
     {
         int avatarSelected = (UserDataManager.Instance.avatar == UserDataManager.Avatar.BORIS) ? 0 : 1;
         
         for (int i = 0; i < rigs.Length; i++)
         {
-            if (avatarSelected != i)
+            if (avatarSelected == i)
             {
-                rigs[i].SetActive(false);
+                rigs[i].SetActive(true);
             }
         }
     }
